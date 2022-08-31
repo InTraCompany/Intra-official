068f7e05810d890464cb7b01bb3c85516f8b0d10
InTraCompany <111281957+InTraCompany@users.noreply.github.com>
2022-08-30 22:18:55 -0300

    cadastro 30-08

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,7 +1,7 @@
 //Caixa vermelha
-function RedBox(box, p, phrase) {
+function RedBox(box, p, phrase, color) {
   let dBox = document.getElementById(box);
-  dBox.style.backgroundColor = "red";
+  dBox.style.backgroundColor = color;
   let dP = document.getElementById(p);
   dP.innerHTML = phrase;
 }
@@ -22,61 +22,77 @@ function ValidateEmail() {
   var inputEmail = document.getElementsByName("email");
   var email = inputEmail.value;
   var exist = AJAX("email", email);
-  var pEmail = document.getElementById("email-isnt-correct");
   if (
     email === undefined ||
     email === null ||
     email[0] === " " ||
     exist === true
   ) {
-    let boxEmail = document.getElementById("email");
-    boxEmail.style.backgroundColor = "red";
-    pEmail.innerHTML =
+    let phrase =
       exist === true ? "Este email já está em uso" : "Email inválido";
+    let pEmail = "email-isnt-correct";
+    let boxEmail = "box-email";
+    let color = "red";
+    RedBox(boxEmail, pEmail, phrase, color);
+    return false;
   } else {
     return true;
   }
 }
 
 //código para validar a primeira senha
-function ValidatePwd() {
-  var pwd = document.getElementById("password").value;
+function ValidatePwd(password) {
+  var pwd = password.value;
   const pPwd = "pwd-isnt-correct";
+  const boxPwd = "box-pwd";
   if (pwd === undefined || pwd == null) {
     let phrase = "Senha inválida";
-    RedBox("pwd", pPwd, phrase);
+    RedBox(boxPwd, pPwd, phrase);
     return false;
   }
   if (pwd.length < 8) {
     let phrase = "Senha muito curta";
-    RedBox("pwd", pPwd, phrase);
+    RedBox(boxPwd, pPwd, phrase);
     return false;
   }
   if (pwd.match("/[0-9]/mu") === false) {
     let phrase =
       "Para sua segurança recomendamos que adicione números a esta senha";
-    RedBox("pwd", pPwd, phrase);
+    RedBox(boxPwd, pPwd, phrase);
     return false;
   }
   if (pwd.match("/[a-z]/mu") === false || pwd.match("/[A-Z]/mu") === false) {
     let phrase =
       "Para sua segurança recomendamos que varie entre letras minúsculas e maiusculas nesta senha";
-    RedBox("pwd", pPwd, phrase);
+    RedBox(boxPwd, pPwd, phrase);
     return false;
   }
   if (pwd.match("/[#$%^&*()+=-[]';,./{}|\":<>?~\\\\]/mu") === false) {
     let phrase =
       "Para sua segurança recomendamos que adicione caracteres especiais a esta senha";
-    RedBox("pwd", pPwd, phrase);
+    RedBox(boxPwd, pPwd, phrase);
     return false;
   }
   return true;
 }
 
-function ValidateCpwd() {
-  var cPwd = document.getElementsByName("cpassword").value;
-  var pCpwd = document.getElementById("cpwd-isnt-correct");
-  if (cPwd === undefined || cPwd == null) {
+function ValidateCpwd(cpwd) {
+  var Cpwd = cpwd.value;
+  var pwd = document.getElementById("password").value;
+  var pCpwd = "cpwd-isnt-correct";
+  var boxCpwd = "box-cpwd";
+  var phrase = "";
+  var color = "white";
+  if (Cpwd !== pwd) {
+    phrase = "Senhas incompatíveis";
+    color = "red";
+    RedBox(boxCpwd, pCpwd, phrase, color);
+    return false;
+  } else {
+    RedBox(boxCpwd, pCpwd, phrase, color);
+    return false;
+  }
+  /*if (cPwd === undefined || cPwd == null) {
     pCpwd.innerHTML = "Senha inválido";
     return false;
   }
@@ -98,29 +114,39 @@ function ValidateCpwd() {
     pCpwd.innerHTML =
       "Para sua segurança recomendamos que adicione caracteres especiais a esta senha";
     return false;
-  }
+  }*/
   return true;
 }
 
 function ValidateRecaptcha() {
   var recap = document.getElementById("recaptcha");
-  if (recap.checked == true) {
+  var boxRecap = "recaptcha-box";
+  var pRecap = "recaptcha-not-accepted";
+  var phrase = "";
+  var color = "white";
+  if (recap.checked === true) {
+    RedBox(boxRecap, pRecap, phrase, color);
     return true;
   } else {
-    let phrase = "É necessário aceitar os termos para continuar";
-    RedBox("recaptcha-box", "recaptcha-not-accepted", phrase);
+    phrase = "É necessário aceitar os termos para continuar";
+    color = "red";
+    RedBox(boxRecap, pRecap, phrase, color);
     return false;
   }
 }
 
 function ValidateTerms() {
   var terms = document.getElementById("terms");
+  var boxTerms = "terms-box";
+  var pTerms = "terms-not-accepted";
+  var phrase = "";
+  var color = "white";
   if (terms.checked === true) {
-    console.log("tems checked");
     return true;
   } else {
-    let phrase = "É necessário aceitar os termos para continuar";
-    RedBox("terms-box", "terms-not-accepted", phrase);
+    phrase = "É necessário aceitar os termos para continuar";
+    color = "red";
+    RedBox(boxTerms, pTerms, phrase, color);
     return false;
   }
 }
